1b90caaced4a9c2f7ff6a0fef5ffee631c01913e
Adam Piper <adam@ahri.net>
2023-10-22 18:01:47 +0000

    Factor out single website check, fix retries.

--- util.sh
+++ util.sh
@@ -1,38 +1,53 @@
-websites_up()
+website_up()
 {
 	local timeout_seconds=2
 	local timeout_retries=10
 
+	if [ $# -ne 1 ]; then
+		echo "Provide a URL to website_up"
+		return 1
+	fi
+	url="$1"
+
+	for i in `seq 1 $timeout_retries`; do
+		http_out=`set +e; curl -m $timeout_seconds -I -w '%{http_code}\n' "$url" 2>&1; echo $?`
+		status=`echo "$http_out" | tail -n2`
+		http_status=`echo "$status" | head -n1`
+		curl_exit=`echo "$status" | tail -n1`
+
+		echo "$url [curl=$curl_exit, http=$http_status]"
+
+		if [ $curl_exit -eq 28 ]; then
+			echo "$url [transient timeout $i, retrying]"
+		elif [ $curl_exit -ne 0 ]; then
+			echo "$url [curl err $curl_exit]"
+			return 1
+		elif [ $http_status -lt 100 ] || [ $http_status -ge 500 ]; then
+			echo "$url [$http_status]"
+			return 1
+		else
+			return 0
+		fi
+	done
+
+	echo "$url [fatal timeout]"
+	return 1
+}
+
+websites_up()
+{
 	if [ $# -eq 0 ]; then
-		echo "Provide at least one URL to websites_up"
+		echo "Provide at least one URL to websites_up" 1>&2
 		return 1
 	fi
 
 	code=0
-	set +e
 	while [ $# -gt 0 ]; do
 		url="$1"
 		shift
-		for i in {1..$timeout_retries}; do
-			http_status=`curl -m $timeout_seconds -Is -o /dev/null -w '%{http_code}' "$url"`
-			curl_exit=$?
-			if [ $curl_exit -eq 28 ]; then
-				# timeout, assume there's a weird network issue and try again
-				code=1
-				continue
-			elif [ $curl_exit -ne 0 ]; then
-				echo "$url [curl err $curl_exit]"
-				code=1
-			elif ! echo "$http_status" | grep -q "^[1-4][0-9][0-9]$"; then
-				echo "$url [$http_status]"
-				code=1
-			fi
-			break
-		done
-	done
-	set -e
 
-	return $code
+		website_up "$url"
+	done
 }
 
 disk_free_above_percentage()
